c1aac9743533e63013beb42dc4a04d9b8a4f759c
Hayversson Jhair Mosquera Moreno <hayversson10one@gmail.com>
2026-05-30 12:39:54 -0500

    feat: initialize rentify-frontend application structure

--- Rentify.DataAccess/Migrations/20260530162652_DbTotal.Designer.cs
+++ Rentify.DataAccess/Migrations/20260530162652_DbTotal.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Rentify.DataAccess.Context;
 
@@ -11,9 +12,11 @@ using Rentify.DataAccess.Context;
 namespace Rentify.DataAccess.Migrations
 {
     [DbContext(typeof(RentifyDbContext))]
-    partial class RentifyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260530162652_DbTotal")]
+    partial class DbTotal
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Rentify.DataAccess/Migrations/20260530162652_DbTotal.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rentify.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class DbTotal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
